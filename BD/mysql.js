5dc0983536268e06097d89802b87b38058789734
Alejandro-veloz-rubio <79126944+Alejandro-veloz-rubio@users.noreply.github.com>
2024-03-31 13:25:38 -0600

    Add files via upload

--- BD/mysql.js
+++ BD/mysql.js
@@ -2,16 +2,22 @@ var mysql      = require('mysql2');
 var hostdb = process.env.HOST || 'localhost';
 var userdb = process.env.USER || 'root';
 var passdb = process.env.PASSWORD || 'Barcelokp2007';
-var datadb = process.env.DATABASE || 'Report_Incidents'
+var datadb = process.env.DATABASE || 'Report_Incidents';
+var portdb = process.env.PORT || '3306';
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+  waitForConnections: true,
+  connectionLimit: 1000,
+  queueLimit: 1000,
   host     : hostdb,
   user     : userdb,
   password : passdb,
-  database : datadb
+  database : datadb,
+  port: portdb,
+  debug: false
 }); //establecemos conexion con la base de datos definiendo algunos parametros
  
-connection.connect(function(err){
+connection.getConnection(function(err){
     if(err){
         console.log(err);//si hubo un error nos lo mostrara
         return;
